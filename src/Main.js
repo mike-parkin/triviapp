d45b68d600ffde9108ae438e4222e75b3e73815a
Mike Parkin <michael.parkin1988@gmail.com>
2021-04-06 20:20:33 +1200

    init commit

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+} from 'react-native'
+
+const Main = () => {
+    return (
+        <View>
+            <Text>Main Component</Text>
+        </View>
+    )
+}
+
+export default Main
