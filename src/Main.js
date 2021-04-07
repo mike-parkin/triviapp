e072cbaa09cd9619cc049523f00cf5856a650755
Mike Parkin <michael.parkin1988@gmail.com>
2021-04-07 15:55:48 +1200

    test api call returning data and rendering

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     StyleSheet,
     ScrollView,
     View,
     Text,
 } from 'react-native'
+import { getQuestions } from './api/openTDb'
+
 
 const Main = () => {
+    const [ questions, setQuestions ] = useState([])
+
+    const fetchQuestions = () => {
+        return getQuestions()
+        .then(response => {
+            console.log(response)
+            setQuestions(response.data.results)
+        })
+    }
+    
+    useEffect(() => {
+        console.log('fetch questions called')
+        fetchQuestions()
+    },[])
+    
+    
+
+
+
     return (
         <View>
             <Text>Main Component</Text>
+            {questions.map(question => {
+                return(
+                    <Text>{question.question}</Text>
+                )
+            })}
         </View>
     )
 }
--- src/api/openTDb.js
+++ src/api/openTDb.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
-const getQuestions = (schema) => {
-    
+const baseUrl = 'https://opentdb.com/api.php?amount=10'
+
+export const getQuestions = () => {
+    return axios.get(baseUrl)
+        .then(response => response)
 }
