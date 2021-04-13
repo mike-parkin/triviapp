be319244f6889b56407813710dd40886a2f47b43
Mike Parkin <michael.parkin1988@gmail.com>
2021-04-13 19:53:37 +1200

    Quizbulder page rendering test passed

--- __tests__/QuizBuilder.Test.js
+++ __tests__/QuizBuilder.Test.js
@@ -2,6 +2,8 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import QuizBuilder from '../components/QuizBuilder'
 
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+
 test('renders correctly', () => {
     const tree = renderer.create(<QuizBuilder />).toJSON()
     expect(tree).toMatchSnapshot()
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    preset: 'react-native',
+    preset: 'jest-expo',
     setupFiles: [
         "./node_modules/react-native-gesture-handler/jestSetup.js"
       ],
     setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
     transformIgnorePatterns: [
-        "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)"
+        "node_modules/(?!((jest-)?react-native|react-navigation|@react-navigation/.*|react-native-picker-select|\@react-native-picker/.*|@expo/.*))"
       ]
   };
