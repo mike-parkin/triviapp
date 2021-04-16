24e9af2c37ff3be0d73a5022e501715a86f9cf19
Mike Parkin <michael.parkin1988@gmail.com>
2021-04-16 17:44:35 +1200

    combine reducers created

--- App.js
+++ App.js
@@ -1,28 +1,34 @@
 import 'react-native-gesture-handler'
-
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
-
+import { Provider } from 'react-redux'
+import { createStore, compose } from 'redux'
+// redux
+import reducers from './reducers'
+// componets
 import MainMenu from './components/MainMenu'
 import QuizBuilder from './components/QuizBuilder'
 import QuizPage from './components/QuizPage'
 import Details from './components/Details'
 
+const store = createStore(reducers)
 const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='MainMenu' component={MainMenu} />
-        <Stack.Screen name='QuizBuilder' component={QuizBuilder} />
-        <Stack.Screen name='QuizPage' component={QuizPage} />
-        <Stack.Screen name='Details' component={Details} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='MainMenu' component={MainMenu} />
+          <Stack.Screen name='QuizBuilder' component={QuizBuilder} />
+          <Stack.Screen name='QuizPage' component={QuizPage} />
+          <Stack.Screen name='Details' component={Details} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
---
+++ reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+
+import quizReducer from './quiz'
+
+const reducers = combineReducers({
+    quizReducer
+})
+
+export default reducers
