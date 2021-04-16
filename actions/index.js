0e30545acc5a8823ee5b4e411159355ecc54b92b
Mike Parkin <michael.parkin1988@gmail.com>
2021-04-16 17:53:11 +1200

    action for adding answers to quiz reducer

---
+++ actions/index.js
@@ -0,0 +1,9 @@
+export const ADD_ANSWER = 'ADD_ANSWER'
+
+export function addAnswer(answer) {
+    return {
+        type: ADD_ANSWER,
+        answer: answer
+    }
+}
+
---
+++ reducers/quiz.js
@@ -0,0 +1,6 @@
+
+const intialState = []
+
+const reducer = (action, state = intialState) => {
+
+}
